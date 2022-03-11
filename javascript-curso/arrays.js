48c9d05af1f3168bbadbc4c42729579cd6afaa3b
Diego D B Thedoldi <diegodallabt01@gmail.com>
2022-03-11 15:08:42 -0300

    Funções para manipular arrays

---
+++ javascript-curso/arrays.js
@@ -0,0 +1,21 @@
+/*
+MANIPULANDO ARRAYS
+
+v = váriavel que representa um array
+
+SPLICE -> Adiciona ou remove elementos de um array 
+v.splice(indice, delete, elm1, elm2, elm3)
+
+CONCAT -> Concatena dois arrays
+v1.concat(v2, v3, v4)
+
+SPREAD OPERATOR -> Desestruturação 
+v3 = [...v1, ...v2]
+
+FILTER -> Filtra um array
+v = [1, 30, 22, 4, 9, 11]
+vFilter = v.filter((valor, indice, array) => {
+    ...
+})
+
+ */
