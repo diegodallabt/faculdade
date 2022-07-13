ee48fb0f85263d7b71ef8ce09e38884712a95fd3
Diego D B Thedoldi <diegodallabt01@gmail.com>
2022-07-13 09:09:31 -0300

    Implementação da função de leitura do BootRecord

--- trabalhos/sistema-de-arquivos/createImage.c
+++ trabalhos/sistema-de-arquivos/createImage.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <math.h>
 
 #define SIZE_DISK 16777216
@@ -20,9 +21,10 @@ typedef struct BootRecord
   unsigned int      total_directory_entries;
 }__attribute__((packed)) BootRecord;
 
-BootRecord setBR(FILE * img){
+bool setBR(FILE * img){
     BootRecord br;
-    
+
+    // seta os valores padrões do boot record
     br.bytes_per_sector = BYTES_PER_SECTOR;
     br.sectors_per_cluster = SECTORS_PER_CLUSTER;
     br.reserved_sector = RESERVED_SECTOR;
@@ -31,16 +33,30 @@ BootRecord setBR(FILE * img){
     br.pointer_to_space_free = 0;
     br.total_directory_entries = 0;
 
+    // posiciona ponteiro no inicio da imagem
     fseek(img, 0, SEEK_SET);
-    fwrite(&br, 1 , sizeof(br) , img);
 
+    // escreve o bootrecord na imagem
+    fwrite(&br, 1 , sizeof(br) , img);
+        
+    return true;
+}
+
+BootRecord readBR(FILE *img){
+    BootRecord br;
+
+    // posiciona o ponteiro
+    fseek(img, 0, SEEK_SET);
+    
+    //lê todo o bootrecord da imagem e armazena
+    fread(&br, sizeof(BootRecord), 1, img);
+    
     return br;
 }
 
-
 void createImage(){
     FILE * img;
-    char nulo[1] = {0};
+    char byte[1] = {0};
 
     img = fopen( "file.img" , "wb" );
 
@@ -49,13 +65,15 @@ void createImage(){
     }
 
     for(int i=0 ; i<= SIZE_DISK ; i++){
-        fwrite(nulo, 1 , sizeof(nulo) , img);
+        fwrite(byte, 1 , sizeof(byte) , img);
     }
-    setBR(img);
+    
+    if(setBR(img)){
+        printf("A imagem foi criada com sucesso e está pronta para uso.\n");
+    }
 
     fclose(img);
 }
-
 
 int main(int argc, char const *argv[])
 {
